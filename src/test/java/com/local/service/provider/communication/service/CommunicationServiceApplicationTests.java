adc8cc670fc813118f2c02738beec901c86e02b6
alokdev08-create <alokjavadev08@gmail.com>
2025-03-22 13:47:49 +0530

    First commit

---
+++ src/test/java/com/local/service/provider/communication/service/CommunicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.service.provider.communication.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
